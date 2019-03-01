65f7736117e6cbdb630b879a282acb71259dc139
Stuart Bell <stuart.bell1982@googlemail.com>
2019-03-01 10:59:11 +0000

    update crud functionality

--- server/server.js
+++ server/server.js
@@ -19,5 +19,5 @@ MongoClient.connect('mongodb://localhost:27017')
   .catch(console.error);
 
 app.listen(3000, function() {
-  console.log(`Hotel server running on port ${this.address().port}`);
+  console.log(`Stocks server running on port ${this.address().port}`);
 });
