83d7645a677b047aa9b6e643fad097d10aa8b1fd
Kyle Johnston <kyle9657@gmail.com>
2019-03-01 14:59:15 +0000

    Added function to total values of all shares held

--- client/src/components/StockView.vue
+++ client/src/components/StockView.vue
@@ -3,7 +3,7 @@
     <ul v-for="stock in stocks">
       <li><p>Your {{ stock.stockName }} shareprice is ${{ stock.closingPrice }} and you have {{ stock.quantity }} with a value of ${{ stock.quantity * stock.closingPrice }}</p></li>
     </ul>
-
+    <p>Your total Portfolio value is ${{totalValue}}</p>
   </div>
 </template>
 
@@ -11,7 +11,17 @@
 export default {
   name: 'StockView',
   props: ['stocks'],
-}
+  computed: {
+    totalValue: function(stock) {
+      let total = 0;
+      for (stock of this.stocks){
+        total += stock.quantity * stock.closingPrice
+      }
+      // console.log(total);
+      return total;
+    }
+    }
+  }
 </script>
 
 <style lang="css" scoped>
