5ad2501c78af54bccb0ce76a6888df440e030707
Kyle Johnston <kyle9657@gmail.com>
2019-03-01 16:17:44 +0000

    Saved stockprices

---
+++ client/src/components/StockPrices.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div class="">
+    <label for="stock_select">Choose a Stock</label>
+    <select id="stock_select" v-model="apiCall" v-on:change="changeStock">
+      <option value="AAPL">Apple Stocks (AAPL)</option>
+      <option value="GOOG">Google Stocks (GOOG)</option>
+    </select>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../main';
+
+export default {
+  name: 'stockPrices',
+  data(){
+    return {
+      apiCall: null
+    }
+  },
+  methods: {
+    changeStock(){
+      console.log(this.apiCall);
+      eventBus.$emit('stock-selected', this.apiCall)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
