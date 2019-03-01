caf7ed493d5a4adaadf6a9f34b81dba10ab6bd94
Kyle Johnston <kyle9657@gmail.com>
2019-03-01 16:43:57 +0000

    Fix for bug where select doesnt update after first select. not working properly

--- client/src/components/StockPrices.vue
+++ client/src/components/StockPrices.vue
@@ -2,6 +2,7 @@
   <div class="">
     <label for="stock_select">Choose a Stock</label>
     <select id="stock_select" v-model="apiCall" v-on:change="changeStock">
+      <option value=disabled></option>
       <option value="AAPL">Apple Stocks (AAPL)</option>
       <option value="GOOG">Google Stocks (GOOG)</option>
     </select>
