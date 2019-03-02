bbd851d85379d04ba89018ef0f29bd99e8882386
Kyle Johnston <kyle9657@gmail.com>
2019-03-02 12:36:29 +0000

    Updated DB and some text

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -4,22 +4,22 @@ db.dropDatabase();
 db.stocks.insertMany([
   {
     stockName: 'GOOG',
-    closingPrice: 1124.43,
-    quantity: 200
+    closingPrice: 10,
+    quantity: 15
+  },
+  {
+    stockName: 'GOOG',
+    closingPrice: 15,
+    quantity: 20
   },
   {
     stockName: 'AAPL',
-    closingPrice: 174.54,
-    quantity: 456
+    closingPrice: 15,
+    quantity: 10
   },
   {
     stockName: 'MSFT',
-    closingPrice: 113.45,
-    quantity: 652
-  },
-  {
-    stockName: 'AMZN',
-    closingPrice: 1646.23,
-    quantity: 98
+    closingPrice: 5,
+    quantity: 10
   }
 ])
