81525bf3bfe430aad232cc37a3c9824c800ac8a3
Kyle Johnston <kyle9657@gmail.com>
2019-03-05 14:29:52 +0000

    rewrote method to total initial purchase value

--- client/src/components/StockView.vue
+++ client/src/components/StockView.vue
@@ -3,32 +3,21 @@
     <ul v-for="stock in stocks">
       <li><p>Your {{ stock.stockName }} shareprice is ${{ stock.closingPrice }} and you have {{ stock.quantity }} shares with a total purchase  value of ${{ stock.quantity * stock.closingPrice }}</p></li>
     </ul>
-    <p>Your total Portfolio value is ${{totalValue}}</p>
+    <p>Your total Portfolio purchase value was ${{totalValue}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'StockView',
-  props: ['stocks', 'groupedTotals'],
+  props: ['stocks', ],
   computed: {
-    totalValue: function() {
-      // var total = 0;
-      // var requestList = this.groupedTotals;
-      //
-      // for(let stock in requestList) {
-      //   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=JUSZH2FOEHQR49T8`)
-      //   .then(res => res.json())
-      //   .then(result => {
-      //     var a = result['Time Series (Daily)']
-      //     var finalKey = Object.keys(a).shift()
-      //     var price = a[finalKey]['4. close']
-      //     var value = price * requestList[stock]
-      //     total += value
-      //   })
-      //   console.log(total);
-      //   return total
-      // }
+    totalPercentage: function() {
+      let totalPurchase = 0;
+      for (var stock of this.stocks){
+        totalPurchase += stock.quantity * stock.closingPrice
+      }
+      return totalPurchase;
     }
   }
 }
