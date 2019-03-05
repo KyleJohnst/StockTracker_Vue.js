e775b8f6bf62d4103d0de3c2a46f0e18f43d51db
Simon McBride <rexnash@gmail.com>
2019-03-05 16:11:12 +0000

    Commit recent updates

---
+++ client/src/components/GraphData.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div id="chart-container">
+    <p> {{ createChartData }} </p>
+    <div class="g-chart">
+      <GChart
+      type="PieChart"
+      :data="chartData"
+      :options="chartOptions"
+      />
+    </div>
+  </div>
+</template>
+<script>
+
+import { Gchart } from 'vue-google-charts';
+
+export default {
+  name: 'GraphData',
+  props: ['groupedTotals'],
+  components: {
+    Gchart
+  },
+  data(){
+    return {
+      chartData: [],
+      chartOptions: {
+        width: 1000,
+        height: 500,
+        colors: ['red', 'blue', 'green', 'orange'],
+        is3D: true,
+        title: 'Stock Holdings',
+      }
+    }
+  },
+  computed: {
+    createChartData(){
+      this.chartData = [["Symbol", "Amount"], ...Object.entries(this.groupedTotals)]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
