5a298505fc685bbe07d3b4ac354937cd1b022b8b
Kyle Johnston <kyle9657@gmail.com>
2019-03-05 14:35:57 +0000

    Function to total inital purchase value

--- client/src/components/StockView.vue
+++ client/src/components/StockView.vue
@@ -3,40 +3,13 @@
     <ul v-for="stock in stocks">
       <li><p>Your {{ stock.stockName }} shareprice is ${{ stock.closingPrice }} and you have {{ stock.quantity }} shares with a total purchase  value of ${{ stock.quantity * stock.closingPrice }}</p></li>
     </ul>
-<<<<<<< HEAD
-    <p>Your total Portfolio value is $</p>
-=======
     <p>Your total Portfolio purchase value was ${{totalValue}}</p>
->>>>>>> fix/automate_features
   </div>
 </template>
 
 <script>
 export default {
   name: 'StockView',
-<<<<<<< HEAD
-  props: ['stocks', 'groupedTotals'],
-  // computed: {
-  //   totalValue: function() {
-      // var total = 0;
-      // var requestList = this.groupedTotals;
-      //
-      // for(let stock in requestList) {
-      //   fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=JUSZH2FOEHQR49T8`)
-      //   .then(res => res.json())
-      //   .then(result => {
-      //     var a = result['Time Series (Daily)']
-      //     var finalKey = Object.keys(a).shift()
-      //     var price = a[finalKey]['4. close']
-      //     var value = price * requestList[stock]
-      //     total += value
-      //   })
-      //   console.log(total);
-      //   return total
-      // }
-    // }
-  // }
-=======
   props: ['stocks', ],
   computed: {
     totalPercentage: function() {
@@ -47,7 +20,6 @@ export default {
       return totalPurchase;
     }
   }
->>>>>>> fix/automate_features
 }
 </script>
 
