262cbf5260b3319ff3173d39b074183fbd1fa905
Kyle Johnston <kyle9657@gmail.com>
2019-03-15 16:22:36 +0000

    Added function that returns the current stock price for each share held. Issue with synchronosity on render

--- client/src/components/StockView.vue
+++ client/src/components/StockView.vue
@@ -1,8 +1,8 @@
 <template lang="html">
   <div class="">
-    <!-- <ul v-for="stock in stocks"> ******* THis could be added in at a later time for more detail
-    <li><p>{{ stock.stockName }} price per share ${{ stock.closingPrice }} Shares held {{ stock.quantity }}</p></li>
-  </ul> -->
+    <ul v-if="stock.Price" v-for="stock in stocks">
+    <li><p>{{ stock.stockName }} price per share ${{ stock.closingPrice }} Shares held {{ stock.quantity }} current share price ${{stock.Price}}</p></li>
+  </ul>
   <p>Current portfolio value ${{totalStockValue}} has changed by {{getPercentage}}%</p>
 </div>
 </template>
@@ -10,16 +10,18 @@
 <script>
 export default {
   name: 'StockView',
-  props: ['totalStockValue', 'initialValue'],
+  props: ['initialValue'],
   data(){
     return {
       stocks: [],
-      updatedStocks: []
+      updatedStocks: [],
+      totalStockValue: null
     }
   },
   mounted(){
     this.fetchStocks()
-    .then(upStock => this.fetchCurrentValue());
+    .then(upStock => this.fetchCurrentValue())
+    .then(cntStock => this.fetchTotalCurrValue())
   },
   computed: {
     getPercentage() {
@@ -28,9 +30,9 @@ export default {
         let currValue = this.totalStockValue
         let initValue = this.initialValue
         let increase = currValue - initValue
-        let x = ((increase / initValue) * 100).toFixed(2)
+        let y = ((increase / initValue) * 100).toFixed(2)
 
-        return x
+        return y
       }
     }
   },
@@ -41,18 +43,27 @@ export default {
       .then(stocks => this.stocks = stocks);
     },
     fetchCurrentValue() {
-      // debugger;
       const key = '&apikey=G0JO6UF2EJDL6UXE'
       for(let stock of this.stocks){
-        console.log(stock);
-        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock['stockName'] + key)
+        let company = stock.stockName
+        fetch('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + company + key)
         .then(res => res.json())
         .then(result => {
-          var a = result['Time Series (Daily)']
-          var finalKey = Object.keys(a).shift()
-          this.stock['Price:']= a[finalKey]['4. close'];
+          let a = result['Time Series (Daily)']
+          let finalKey = Object.keys(a).shift()
+          stock['Price']= a[finalKey]['4. close'];
         })
       }
+    },
+    fetchTotalCurrValue(){
+      let value = 0;
+
+      for (let stock of this.stocks){
+        if (stock.Price) {
+          value += stock.quantity * stock.Price
+        }
+      }
+      this.totalStockValue = value
     }
   }
 }
